f06941c0d86853bcb4797b26cca2ea4b8cd65a30
Stephan Frolov <stefro97@yandex.ru>
2023-10-16 17:27:29 +0300

    Fixing Logo

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -6,7 +6,7 @@ import styles from "./Logo.module.css";
 export default function Logo() {
   return (
     <Link to="/" className={styles.logo}>
-      <img src="../../../public/logo.png" alt="vanlife" />
+      <img src="logo.png" alt="vanlife" />
     </Link>
   );
 }
